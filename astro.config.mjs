898bf3db4e730aa04d2fdd0eddbd9eb5e7fa15d1
Torben Haack <t128n@ipv4.8shield.net>
2025-07-20 17:07:10 +0200

    feat: add Astro integrations (mdx, icon, mcp), update config, and enable alias paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import mcp from "astro-mcp";
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [mcp(), mdx(), icon()],
+    vite: {
+        css: {
+            transformer: "lightningcss",
+        },
+    },
+});
