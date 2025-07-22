ea8f0300a426f9adaccf08be7fe6f599de77db02
Torben Haack <t128n@ipv4.8shield.net>
2025-07-22 19:40:03 +0200

    feat: configure dynamic site URL based on environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,15 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import mcp from "astro-mcp";
 
+
+const productionMode = process.env.NODE_ENV === "production";
+const astroSite = productionMode
+	? "https://t128n.github.io"
+	: "http://localhost:4321";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://t128n.github.io",
+	site: astroSite,
 	integrations: [mcp(), mdx(), icon(), sitemap()],
 	markdown: {
 		shikiConfig: {
