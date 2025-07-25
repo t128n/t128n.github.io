90900b169b3a262095e88cdcd599024e643566ac
Torben Haack <t128n@ipv4.8shield.net>
2025-07-25 17:52:17 +0200

    chore: update package.json to include astro-compress dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,19 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import compress from "astro-compress";
 import icon from "astro-icon";
 import mcp from "astro-mcp";
 
-
 const productionMode = process.env.NODE_ENV === "production";
 const astroSite = productionMode
-	? "https://t128n.github.io"
-	: "http://localhost:4321";
+	? "https://t128n.github.io/"
+	: "http://localhost:4321/";
 
 // https://astro.build/config
 export default defineConfig({
 	site: astroSite,
-	integrations: [mcp(), mdx(), icon(), sitemap()],
+	integrations: [mcp(), mdx(), icon(), sitemap(), compress()],
 	markdown: {
 		shikiConfig: {
 			theme: "vitesse-dark",
